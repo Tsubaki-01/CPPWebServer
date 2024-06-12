aedb5d44b37371c630389ac41710643c5eac1852
Tsubaki-01 <wengqiang9198@gmail.com>
2024-06-12 16:09:46 +0800

    add Makefile

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "./server/webserver.h"
+
+int main()
+{
+    WebServer server(1316, 3, 60000, false,
+        3306, "root", "pwd", "wenserver",
+        12, 6, true, 1, 1024);
+
+    server.start();
+}
